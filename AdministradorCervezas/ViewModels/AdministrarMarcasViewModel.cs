1f79b8b86596435241775f9068288d5fd6526f45
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-02-06 22:08:29 -0800

    Agregar Marca WIP (Work In Progress)

---
+++ AdministradorCervezas/ViewModels/AdministrarMarcasViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministradorCervezas.ViewModels
+{
+    class AdministrarMarcasViewModel : Screen
+    {
+        private string  _nombrePais;
+
+        public string  NombrePais
+        {
+            get { return _nombrePais; }
+            set { _nombrePais = value; }
+        }
+
+        private string _nombreMarca;
+
+        public string NombreMarca
+        {
+            get { return _nombreMarca; }
+            set { _nombreMarca = value; }
+        }
+
+        public bool PuedeGuardar
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(NombrePais) && !string.IsNullOrWhiteSpace(NombreMarca);
+            }
+        }
+
+        public bool PuedeReiniciar
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(NombrePais) || !string.IsNullOrWhiteSpace(NombreMarca);
+            }
+        }
+
+        public bool PuedesEscribirNombrePais
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(NombreMarca);
+            }
+        }
+    }
+}
