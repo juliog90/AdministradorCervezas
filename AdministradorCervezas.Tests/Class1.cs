3cc038d6ebaeed654a059526f6a8ac06ce051045
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-02-12 22:30:13 -0800

    NUnit agregado

---
+++ AdministradorCervezas.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministradorCervezas.Tests
+{
+    [TestFixture]
+    public class BeerModel
+    {
+        [Test]
+        public void CervezaIDBaseDatos()
+        {
+            Beer cervezaPrueba = new Beer(1);
+            Assert.IsNotNull(cervezaPrueba);
+        }
+    }
+}
