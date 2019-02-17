3b0af4189a906badfd9e14b7338cac51b26f5e23
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-02-16 22:28:18 -0800

    pruebas en cervezas y conexion base de datos

---
+++ AdministradorCervezas.Tests/Integracion/BeerTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministradorCervezas.Tests.Integracion
+{
+    [TestFixture]
+    public class BeerTest
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void ObtenerCervezaConId(int value)
+        {
+            Beer cervezaPrueba = new Beer(value);
+            Assert.IsTrue(cervezaPrueba.Id != 0, $"La id {value} no existe en la base de datos");
+        }
+
+        [Test]
+        public void TodasLasCervezas()
+        {
+            List<Beer> cervezas = Beer.GetAll();
+            Assert.IsTrue(cervezas.Count > 0, $"No existen cervezas en la base de datos");
+        }
+
+        [Test]
+        public void CrearCervezaNueva()
+        {
+            Beer cerveza = new Beer();
+            cerveza.Brand = new Brand("HEN");
+            cerveza.Clasification = new Clasification("RED");
+            cerveza.Content = 40;
+            cerveza.Fermlevel = Fermentation.Alto;
+            cerveza.GradoAlcohol = 40;
+            cerveza.Price = 40;
+            cerveza.Image = "nobeer1.png";
+            cerveza.Presentation = PresentationType.Bottle;
+
+            Assert.IsTrue(cerveza.Add(), $"No se pueden crear cervezas");
+        }
+    }
+}
---
+++ AdministradorCervezas.Tests/Integracion/DatabaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AdministradorCervezas.Tests.Integracion
+{
+    [TestFixture]
+    class DatabaseTest
+    {
+        [Test]
+        public void BaseDeDatosDisponible()
+        {
+            MySqlConnection connection = new MySqlConnection();
+            connection.ConnectionSource = new AppSettings();
+            connection.Connect();
+
+            Assert.IsTrue(connection.Open(), $"Base de datos no disponible");
+        }
+    }
+}
