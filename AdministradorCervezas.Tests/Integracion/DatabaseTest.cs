09e3f5606487cee0caf5a2d502d77303a50bc289
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-02-17 21:54:49 -0800

    test de integracion de base de datos

--- AdministradorCervezas.Tests/Integracion/DatabaseTest.cs
+++ AdministradorCervezas.Tests/Integracion/DatabaseTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace AdministradorCervezas.Tests.Integracion
 {
@@ -18,6 +20,32 @@ namespace AdministradorCervezas.Tests.Integracion
             connection.Connect();
 
             Assert.IsTrue(connection.Open(), $"Base de datos no disponible");
+        }
+
+        [TestCase("brand")]
+        [TestCase("beer")]
+        [TestCase("country")]
+        public void EjecutarQuery(string tablaBaseDatos)
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionSource = new AppSettings();
+            conexion.Connect();
+            MySqlCommand comando = new MySqlCommand("select * from " + tablaBaseDatos);
+
+            DataTable tabla = conexion.ExecuteQuery(comando);
+
+            Assert.IsTrue(tabla.Rows.Count > 0);
+        }
+
+        [Test]
+        public void EjecutarNoQuery()
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionSource = new AppSettings();
+            conexion.Connect();
+            MySqlCommand comando = new MySqlCommand("update beer set be_content = 50 where be_id = 1");
+
+            Assert.IsTrue(conexion.ExecuteNonQuery(comando));
         }
     }
 }
