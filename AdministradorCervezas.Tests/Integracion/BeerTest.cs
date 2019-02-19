6bbcc589bccbee4910a74fa02e96a7b6442dab7a
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-02-18 19:51:45 -0800

    pruebas marca

--- AdministradorCervezas.Tests/Integracion/BeerTest.cs
+++ AdministradorCervezas.Tests/Integracion/BeerTest.cs
@@ -12,6 +12,10 @@ namespace AdministradorCervezas.Tests.Integracion
     [TestFixture]
     public class BeerTest
     {
+        /// <summary>
+        /// Verifica que se puede consultar una cerveza
+        /// </summary>
+        /// <param name="value">Id de la base de datos</param>
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(3)]
@@ -20,30 +24,40 @@ namespace AdministradorCervezas.Tests.Integracion
             Beer cerveza = new Beer(value);
             bool existeDatoMarca = cerveza.Brand != null;
             bool existeDatoClas = cerveza.Clasification != null;
-            bool existeDatoCont = cerveza.Content < 0;
-            bool existeDatoFerme = cerveza.Fermlevel < 0;
-            bool existeDatoGrad = cerveza.GradoAlcohol < 0;
-            bool existeDatoPre = cerveza.Price < 0;
+            bool existeDatoCont = cerveza.Content > 0;
+            bool existeDatoFerme = cerveza.Fermlevel >= 0;
+            bool existeDatoGrad = cerveza.GradoAlcohol >= 0;
+            bool existeDatoPre = cerveza.Price > 0;
             bool existeDatoIma = cerveza.Image != null;
-            bool existeDatoPres = cerveza.Presentation < 0;
+            bool existeDatoPres = cerveza.Presentation >= 0;
+            bool existeDatoId = cerveza.Id > 0;
 
             bool datosCorrectos = existeDatoMarca && existeDatoClas && existeDatoCont && existeDatoFerme
-                                  && existeDatoGrad && existeDatoPre && existeDatoIma && existeDatoPres;
+                                  && existeDatoGrad && existeDatoPre && existeDatoIma && existeDatoPres
+                                  && existeDatoId;
 
-            Assert.IsTrue(cerveza.Id != 0, $"La id {value} no existe en la base de datos");
+            Assert.IsTrue(datosCorrectos, $"La id {value} no existe en la base de datos");
         }
 
+        /// <summary>
+        /// Verifica que existan cervezas
+        /// </summary>
         [Test]
         public void TodasLasCervezas()
         {
             List<Beer> cervezas = Beer.GetAll();
-            Assert.IsTrue(cervezas.Count > 0, $"No existen cervezas en la base de datos");
+
+            bool existenCervezas = cervezas.Count > 0;
+
+            Assert.IsTrue(existenCervezas, $"No existen cervezas en la base de datos");
         }
 
+        /// <summary>
+        /// Verifica que se inserte una nueva cerveza en la base de datos
+        /// </summary>
         [Test]
         public void CrearCervezaNueva()
         {
-            // TODO: Verfificar si esta cerveza tiene todos los campos
             Beer cerveza = new Beer();
             cerveza.Brand = new Brand("HEN");
             cerveza.Clasification = new Clasification("RED");
@@ -55,35 +69,20 @@ namespace AdministradorCervezas.Tests.Integracion
             cerveza.Presentation = PresentationType.Bottle;
 
             bool cervezaAgregada = cerveza.Add();
-
-            MySqlConnection conexion = new MySqlConnection();
-            conexion.ConnectionSource = new AppSettings();
-            conexion.Connect();
-            MySqlCommand comando = new MySqlCommand("select be_id as ultima_id from beer order by be_id desc limit 1;");
-            DataTable tabla = conexion.ExecuteQuery(comando);
-            DataRow row = tabla.Rows[0];
-            int ultimoId = (int)row["ultima_id"];
+            int ultimoId = ObtenerUltimaID();
 
             Beer ultima = new Beer(ultimoId);
             cerveza.Id = ultima.Id;
 
-            bool mismaMarca = cerveza.Brand.Id == ultima.Brand.Id;
-            bool mismoPais = cerveza.Brand.Country.Id == ultima.Brand.Country.Id;
-            bool mismoTipo = cerveza.Clasification.BeerType.Id == ultima.Clasification.BeerType.Id;
-            bool mismaClasificacion = cerveza.Clasification.Code == ultima.Clasification.Code;
-            bool mismoContenido = cerveza.Content == ultima.Content;
-            bool mismaFermentacion = cerveza.Fermlevel == ultima.Fermlevel;
-            bool mismoGrado = cerveza.GradoAlcohol == ultima.GradoAlcohol;
-            bool mismoPrecio = cerveza.Price == ultima.Price;
-            bool mismaImagen = cerveza.Image == ultima.Image;
-            bool mismaPresentacion = cerveza.Presentation == ultima.Presentation;
+            bool sonIguales = ComparaCervezas(cerveza, ultima);
 
-
-            Assert.IsTrue(cervezaAgregada && mismaMarca && mismaClasificacion && mismoContenido && mismaFermentacion &&
-                          mismoGrado && mismoPrecio && mismaImagen && mismaPresentacion && mismoPais && mismoTipo,
-                          $"No se pueden crear cervezas");
+            Assert.IsTrue(cervezaAgregada && sonIguales, $"No se pueden crear cervezas");
         }
 
+        /// <summary>
+        /// Verifica que se puedan actualizar cervezas
+        /// </summary>
+        /// <param name="id">Id de la cerveza en la base de datos</param>
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(5)]
@@ -92,13 +91,92 @@ namespace AdministradorCervezas.Tests.Integracion
         public void ActualizarCerveza(int id)
         {
             Beer cerveza = new Beer(id);
+
+            // guardamos informacion para restablecer
+            Beer guardaInfo = new Beer(id);
+
+            // cambiamos datos
             cerveza.Price = 500;
+            cerveza.Brand = new Brand("IND");
+            cerveza.Clasification = new Clasification("AMB");
+            cerveza.Content = 99;
+            cerveza.Fermlevel = Fermentation.Espontaneo;
+            cerveza.MeasurementUnit = MeasurementUnit.ML;
 
             // TODO: Verificar si se edito en cada campo
             // TODO: Terminar Querys
 
-            Assert.IsTrue(cerveza.Edit(), $"No se puede editar");
+            bool siEdito = cerveza.Edit();
+
+            Assert.IsTrue(siEdito, $"No se puede editar");
+
+            // regresamos valores
+            guardaInfo.Edit();
 
         }
+
+        /// <summary>
+        /// Verifica si se puede eliminar una cerveza
+        /// </summary>
+        [Test]
+        public void EliminarCerveza()
+        {
+            int ultimaId = ObtenerUltimaID();
+            Beer cerveza = new Beer(ultimaId);
+
+            bool seElimino = cerveza.Delete();
+            // confirmamos
+            Beer cervezaEliminada = new Beer(ultimaId);
+
+            bool noExiste = cervezaEliminada.Id < 1;
+
+            Assert.IsTrue(seElimino && noExiste, $"No elimino de la base de datos");
+        }
+
+        #region MetodosPrivados
+
+        /// <summary>
+        /// Compara una cerveza con otra
+        /// </summary>
+        /// <param name="cerveza">Cerveza a comparar</param>
+        /// <param name="otraCerveza">Cerveza con que se compara</param>
+        /// <returns>Si es la misma cerveza</returns>
+        private bool ComparaCervezas(Beer cerveza, Beer otraCerveza)
+        {
+            bool mismaMarca = cerveza.Brand.Id == otraCerveza.Brand.Id;
+            bool mismoPais = cerveza.Brand.Country.Id == otraCerveza.Brand.Country.Id;
+            bool mismoTipo = cerveza.Clasification.BeerType.Id == otraCerveza.Clasification.BeerType.Id;
+            bool mismaClasificacion = cerveza.Clasification.Code == otraCerveza.Clasification.Code;
+            bool mismoContenido = cerveza.Content == otraCerveza.Content;
+            bool mismaFermentacion = cerveza.Fermlevel == otraCerveza.Fermlevel;
+            bool mismoGrado = cerveza.GradoAlcohol == otraCerveza.GradoAlcohol;
+            bool mismoPrecio = cerveza.Price == otraCerveza.Price;
+            bool mismaImagen = cerveza.Image == otraCerveza.Image;
+            bool mismaPresentacion = cerveza.Presentation == otraCerveza.Presentation;
+
+            bool sonIguales = mismaMarca && mismoPais && mismoTipo && mismaClasificacion &&
+                              mismoContenido && mismaFermentacion && mismoGrado && mismoPrecio &&
+                              mismaImagen && mismaPresentacion;
+
+            return sonIguales;
+        }
+
+        /// <summary>
+        /// Obtiene la ultima id insertada en la base de datos
+        /// </summary>
+        /// <returns></returns>
+        private int ObtenerUltimaID()
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionSource = new AppSettings();
+            conexion.Connect();
+            MySqlCommand comando = new MySqlCommand("select be_id as ultima_id from beer order by be_id desc limit 1;");
+            DataTable tabla = conexion.ExecuteQuery(comando);
+            DataRow row = tabla.Rows[0];
+            int ultimoId = (int)row["ultima_id"];
+            return ultimoId;
+        }
+
+        #endregion
     }
 }
---
+++ AdministradorCervezas.Tests/Integracion/BrandTest.cs
@@ -0,0 +1,163 @@
+﻿using MySql.Data.MySqlClient;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administradormarcas.Tests.Integracion
+{
+    [TestFixture]
+    public class BrandTest
+    {
+        /// <summary>
+        /// Verifica que se puede consultar una marca
+        /// </summary>
+        /// <param name="value">Id de la base de datos</param>
+        [TestCase("BRA")]
+        [TestCase("COR")]
+        [TestCase("IND")]
+        public void ObtenermarcaConId(string value)
+        {
+            Brand marca = new Brand(value);
+            bool existeDatoPais = marca.Country != null;
+            bool existeDatoNombre = marca.Name != "";
+            bool existeDatoId = marca.Id != "";
+
+            bool datosCorrectos = existeDatoPais && existeDatoNombre && existeDatoId;
+
+            Assert.IsTrue(datosCorrectos, $"La id {value} no existe en la base de datos");
+        }
+
+        /// <summary>
+        /// Verifica que existan marcas
+        /// </summary>
+        [Test]
+        public void TodasLasmarcas()
+        {
+            List<Brand> marcas = Brand.GetAll();
+
+            bool siExiste = marcas.Count > 0;
+
+            Assert.IsTrue(siExiste, $"No existen marcas en la base de datos");
+        }
+
+        /// <summary>
+        /// Verifica que se inserte una nueva marca en la base de datos
+        /// </summary>
+        [Test]
+        public void CrearMarcaNueva()
+        {
+            Brand marca = new Brand();
+            marca.Country = new Country("MEX");
+            marca.Name = "LevantaMuertos";
+            marca.Id = "LMS";
+
+            bool marcaAgregada = marca.Add();
+            string ultimoId = ObtenerUltimaID();
+
+            Brand ultima = new Brand(ultimoId);
+            marca.Id = ultima.Id;
+
+            bool sonIguales = ComparaMarcas(marca, ultima);
+
+            Assert.IsTrue(marcaAgregada && sonIguales, $"No se pueden crear marcas");
+        }
+
+        /// <summary>
+        /// Verifica que se puedan actualizar marcas
+        /// </summary>
+        /// <param name="id">Id de la marca en la base de datos</param>
+        [TestCase("TEC")]
+        [TestCase("SKL")]
+        [TestCase("MOD")]
+        public void Actualizarmarca(string id)
+        {
+            Brand marca = new Brand(id);
+
+            // guardamos informacion para restablecer
+            Brand guardaInfo = new Brand(id);
+
+            // cambiamos datos
+            marca.Country = new Country("MEX");
+            marca.Name = "Four Loko";
+
+            // TODO: Verificar si se edito en cada campo
+            // TODO: Terminar Querys
+
+            bool siEdito = marca.Edit();
+
+            Assert.IsTrue(siEdito, $"No se puede editar");
+
+            // regresamos valores
+            guardaInfo.Edit();
+
+        }
+
+        /// <summary>
+        /// Verifica si se puede eliminar una marca
+        /// </summary>
+        [Test]
+        public void EliminarMarca()
+        {
+            string ultimaId = ObtenerUltimaID();
+            Brand marca = new Brand(ultimaId);
+
+            bool seElimino = marca.Delete();
+            // confirmamos
+            Brand marcaEliminada = new Brand(ultimaId);
+
+            bool noExiste = marcaEliminada.Id == "";
+
+            Assert.IsTrue(seElimino && noExiste, $"No elimino de la base de datos");
+        }
+
+        #region MetodosPrivados
+
+        /// <summary>
+        /// Compara una marca con otra
+        /// </summary>
+        /// <param name="marca">marca a comparar</param>
+        /// <param name="otraMarca">marca con que se compara</param>
+        /// <returns>Si es la misma marca</returns>
+        private bool ComparaMarcas(Brand marca, Brand otraMarca)
+        {
+            bool mismaMarca = marca.Brand.Id == otraMarca.Brand.Id;
+            bool mismoPais = marca.Brand.Country.Id == otraMarca.Brand.Country.Id;
+            bool mismoTipo = marca.Clasification.BrandType.Id == otraMarca.Clasification.BrandType.Id;
+            bool mismaClasificacion = marca.Clasification.Code == otraMarca.Clasification.Code;
+            bool mismoContenido = marca.Content == otraMarca.Content;
+            bool mismaFermentacion = marca.Fermlevel == otraMarca.Fermlevel;
+            bool mismoGrado = marca.GradoAlcohol == otraMarca.GradoAlcohol;
+            bool mismoPrecio = marca.Price == otraMarca.Price;
+            bool mismaImagen = marca.Image == otraMarca.Image;
+            bool mismaPresentacion = marca.Presentation == otraMarca.Presentation;
+
+            bool sonIguales = mismaMarca && mismoPais && mismoTipo && mismaClasificacion &&
+                              mismoContenido && mismaFermentacion && mismoGrado && mismoPrecio &&
+                              mismaImagen && mismaPresentacion;
+
+            return sonIguales;
+        }
+
+        /// <summary>
+        /// Obtiene la ultima id insertada en la base de datos
+        /// </summary>
+        /// <returns></returns>
+        private string ObtenerUltimaID()
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionSource = new AppSettings();
+            conexion.Connect();
+            MySqlCommand comando = new MySqlCommand("select be_id as ultima_id from Brand order by be_id desc limit 1;");
+            DataTable tabla = conexion.ExecuteQuery(comando);
+            DataRow row = tabla.Rows[0];
+            string ultimoId = (string)row["ultima_id"];
+            return ultimoId;
+        }
+
+        #endregion
+    }
+}
