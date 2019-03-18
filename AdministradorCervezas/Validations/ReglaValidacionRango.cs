10de55148a291cad1fc5da75350dc5c17116b9ab
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-03-18 12:02:14 -0700

    estilo validacion

---
+++ AdministradorCervezas/Validations/ReglaValidacionRango.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AdministradorCervezas.Validations
+{
+    public class ReglaValidacionRango : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var age = 0;
+
+            try
+            {
+                if (((string)value).Length > 0)
+                    age = int.Parse((string)value);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, "Illegal characters or " + e.Message);
+            }
+
+            if ((age < Min) || (age > Max))
+            {
+                return new ValidationResult(false,
+                    "Introduce una cantidad en el rango..." + Min + " - " + Max + ".");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+    
+}
